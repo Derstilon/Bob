8c4aceb3ef4de14c4b5d205d54761f1fa378ec5d
Jakub Niechaj <quban123@gmail.com>
2023-10-04 17:03:56 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,111 @@
+﻿var fs = require("fs");
+var http = require("http");
+var socketio = require("socket.io")
+
+//MongoDB
+var mongoose = require("mongoose")
+var Models = require("./database/Models.js")(mongoose)
+mongoose.connect('mongodb://localhost/testKlasaGrupa');
+var Operations = require("./database/Operations.js")
+var db;
+var opers = new Operations();
+//MongoDB
+var server = http.createServer(function (req, res) {
+    console.log(req.method, req.url)
+    switch (req.method) {
+        case "GET": {
+            console.log(req.url.split(".")[1])
+            if (req.url == "/") {
+                get_file("/index.html", 'text/html', res)
+            }
+            else if (req.url == "/favicon.ico") {
+
+            }
+            else {
+                switch (req.url.split(".")[1]) {
+                    case "js": {
+                        get_file(req.url, "application/javascript", res)
+                        break;
+                    }
+                    case "jpeg": {
+                        get_file(req.url, "application/javascript", res)
+                        break;
+                    }
+                    case "css": {
+                        get_file(req.url, "text/css", res)
+                        break;
+                    }
+                    case "html": {
+                        get_file(req.url, "text/html", res)
+                        break;
+                    }
+                }
+            }
+            break;
+        }
+        case "POST": {
+            break;
+        }
+    }
+})
+
+server.listen(3000);
+console.log("serwer startuje na porcie 3000 - ten komunikat zobaczysz tylko raz")
+
+function connectToMongo() {
+
+    db = mongoose.connection;
+
+    //przy wystąpieniu błędu
+
+    db.on("error", function () {
+        console.log("problem z mongo")
+    });
+
+    //przy poprawnym połączeniu z bazą
+
+    db.once("open", function () {
+        console.log("mongo jest podłączone - można wykonywać operacje na bazie");
+    });
+
+    //przy rozłączeniu z bazą
+
+    db.once("close", function () {
+        console.log("mongodb zostało odłączone");
+    });
+}
+
+connectToMongo();
+
+var io = socketio.listen(server)
+
+io.sockets.on("connection", function (client) {
+    client.emit("onconnect", {
+        clientName: client.id
+    })
+    client.on("disconnect", function () {
+        console.log("klient się rozłącza")
+    })
+    client.on("klocek", function (data) {
+        client.broadcast.emit("klocek", { klocekID: data.klocekID, klocekY: data.klocekY, klocekZ: data.klocekZ, klocekX: data.klocekX, klocekR: data.klocekR, wybrany: data.wybrany });
+    })
+    client.on("akcja", function (data) {
+        client.broadcast.emit("akcja", { id: data.id, akcja: data.akcja, wybrany: data.wybrany, klocekR: data.klocekR });
+    })
+})
+
+function get_file(url, method, res) {
+    fs.readFile("static" + url, function (error, data) {
+
+        if (error) {
+            res.writeHead(404, { 'Content-Type': method });
+            res.write("<h1>błąd 404 - nie ma pliku!<h1>");
+            res.end();
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': method })
+            res.write(data)
+            res.end()
+        }
+    })
+}
---
+++ static/js/UI.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="Game.js" />
+/// <reference path="../lib/three.js" />
+/*
+    UI - obsługa interfejsu użytkownika
+*/
+
+function Ui() {
+
+    /*document.getElementById("bt1")
+                    .addEventListener("click", function () {
+                        game.setTest(12);
+
+                    })
+
+    document.getElementById("bt2")
+                .addEventListener("click", function () {
+                    alert("pobieram zmienną test z klasy Game: " + game.getTest());
+
+                })
+
+    document.getElementById("bt3")
+                .addEventListener("click", function () {
+                    net.sendData();
+
+                })*/
+    document.getElementById("camera")
+                .addEventListener("change", function () {
+                    game.camera_move(this.value)
+                })
+    document.getElementById("loguj")
+        .addEventListener("click", function () {
+        var username = document.getElementById("username").value;
+        net.sendData("DODAJ_UZYTKOWNIKA", username);
+    });
+
+    document.getElementById("reset")
+        .addEventListener("click", function () {
+        var username = document.getElementById("username").value;
+        net.sendData("RESET", username);
+
+
+    });
+}
